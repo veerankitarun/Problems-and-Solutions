4591c959eeb046f3568407c0e5c2991c7b21fa7f
Veeranki Tarun Kumar Goud <61703896+veerankitarun@users.noreply.github.com>
2022-08-02 15:06:40 +0530

    updated

--- file2.cpp
+++ file2.cpp
@@ -243,6 +243,85 @@ int main() {
 	printf("\n");
 	return 0;
 }
+
+// Q 11 
+// Write a C program to read an amount (integer value) and break the amount into smallest possible number of bank notes. Go to the editor
+
+//                          Test Data :
+// Input the amount: 375
+//                       Expected Output:
+// There are:
+// 3 Note(s) of 100.00
+// 1 Note(s) of 50.00
+// 1 Note(s) of 20.00
+// 0 Note(s) of 10.00
+// 1 Note(s) of 5.00
+// 0 Note(s) of 2.00
+// 0 Note(s) of 1.00
+
+
+
+#include <stdio.h>
+int main() {
+	int amt, total;
+	printf("Input the amount: ");
+	scanf("%d",&amt);     
+	
+	total = (int)amt/100;
+	printf("There are: ");
+	
+	printf("\n%d Note(s) of 100.00\n", total);
+	amt = amt-(total*100);
+	
+	total = (int)amt/50;
+	printf("%d Note(s) of 50.00\n", total);
+	amt = amt-(total*50);
+	
+	total = (int)amt/20;
+	printf("%d Note(s) of 20.00\n", total);
+	amt = amt-(total*20);
+	
+	total = (int)amt/10;
+	printf("%d Note(s) of 10.00\n", total);
+	amt = amt-(total*10);
+	
+	total = (int)amt/5;
+	printf("%d Note(s) of 5.00\n", total);
+	amt = amt-(total*5);
+	
+	total = (int)amt/2;
+	printf("%d Note(s) of 2.00\n", total);
+	amt = amt-(total*2);
+	
+	total = (int)amt/1;
+	printf("%d Note(s) of 1.00\n", total);
+	return 0;
+}
+
+Q 12
+	// Write a C program to convert a given integer (in seconds) to hours, minutes and seconds. Go to the editor
+//                          Test Data :
+// Input seconds: 25300
+//                       Expected Output:
+// There are:
+// H:M:S - 7:1:40
+#include <stdio.h>
+int main() {
+	int sec, h, m, s;
+	printf("Input seconds: ");
+	scanf("%d", &sec);
+	
+	h = (sec/3600); 
+	
+	m = (sec -(3600*h))/60;
+	
+	s = (sec -(3600*h)-(m*60));
+	
+	printf("H:M:S:h- %d:%d:%d\n",h,m,s);
+	
+	return 0;
+}
+
 	
 
 
